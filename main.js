23ae09cc8436aecbba17b77a936933b025ff6bee
Dana <markova.dana@email.cz>
2022-02-20 09:35:27 +0100

    ukol1,2

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
 // tady je místo pro náš program
+
+
+
+function setBold(element) {
+    element.style.fontWeight = "bold";
+}
+
+function setNormal(element) {
+    element.style.fontWeight = "normal";
+}
+
+
+
+function upravBarvuPisma() {
+    let odstavec = document.querySelector('p');
+    odstavec.style.color = 'red';
+
+}
+
+function upravVelikostPisma() {
+    let odstavec = document.querySelector('p');
+    let computedSize = window.getComputedStyle(odstavec, null).getPropertyValue('font-size');
+    odstavec.style.fontSize = (parseFloat(computedSize) + 1) + 'px';
+
+}
+
+
+
+
+
