1d91f2e5f483589a6aa6c74e987f94d903027b9a
Dana <markova.dana@email.cz>
2022-02-20 10:31:23 +0100

    kompletni

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // tady je místo pro náš program
 
 
-
+// úkol v hodině - vyřešeno v hodině 
 function setBold(element) {
     element.style.fontWeight = "bold";
 }
@@ -11,13 +11,14 @@ function setNormal(element) {
 }
 
 
-
+// úkol č. 1 - jak po dalším kliknutí vrátit na původní barvu textu ?? 
 function upravBarvuPisma() {
     let odstavec = document.querySelector('p');
     odstavec.style.color = 'red';
 
 }
 
+//  úkol č. 2 - využito řešení z 1. lekce
 function upravVelikostPisma() {
     let odstavec = document.querySelector('p');
     let computedSize = window.getComputedStyle(odstavec, null).getPropertyValue('font-size');
@@ -25,7 +26,34 @@ function upravVelikostPisma() {
 
 }
 
+// úkol č. 3
 
+function startAudio() {
+    let audioFile = document.getElementById('zvuk');
+    audioFile.play();
+}
 
+function pauzaAudio() {
+    let audioFile = document.getElementById('zvuk');
+    audioFile.pause();
+}
 
+function ztisitAudio() {
+    let audioFile = document.getElementById('zvuk');
+    audioFile.volume = 0;
+}
 
+function akoratAudio() {
+    let audioFile = document.getElementById('zvuk');
+    audioFile.volume = 0.5;
+}
+
+function maximumAudio() {
+    let audioFile = document.getElementById('zvuk');
+    audioFile.volume = 1;
+}
+
+function zacatekAudio() {
+    let audioFile = document.getElementById('zvuk');
+    audioFile.currentTime = 0;
+}
